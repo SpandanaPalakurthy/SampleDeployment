268b27c7957123fffb7763ff0e018d58d62abb43
SpandanaPalakurthy <50132713+SpandanaPalakurthy@users.noreply.github.com>
2020-07-15 17:38:34 +0530

    commit

---
+++ SampleDeployment/src/main/java/com/cg/SampleDeployment/App.java
@@ -0,0 +1,11 @@
+package com.cg.SampleDeployment;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+	public static void main( String[] args )
+    {
+       System.out.println("Welcome  to FoodDelivery");
+    }
+}
